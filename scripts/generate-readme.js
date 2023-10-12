ff048b09016504158463938b747ee46a81e8790f
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-12 13:13:55 -0400

    Fix generate README script

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -4,55 +4,43 @@ const yaml = require('js-yaml');
 try {
   const doc = yaml.load(fs.readFileSync('./openapi/openapi.yaml', 'utf8'));
 
-  // Extract the title from the OpenAPI document
+  // Define apiTitle with your API title from the OpenAPI file
   const apiTitle = doc.info.title;
 
-  // Extract tags from the OpenAPI file
-  const tags = doc.tags.map((tag) => tag.name);
+  // Extract paths from the OpenAPI file
+  const paths = Object.keys(doc.paths);
 
-  // Generate a table of contents for paths based on tags
-  const tableOfContents = tags.map((tag) => {
-    const tagPaths = Object.entries(doc.paths)
-      .filter(([path, pathData]) => pathData.get.tags.includes(tag))
-      .map(([path, pathData]) => {
-        const operations = Object.keys(pathData);
-        return `- [${tag}](#${tag.toLowerCase()}-${path.replace(/\//g, '-').replace(/{/g, '').replace(/}/g, '').toLowerCase()}) (${operations.join(', ')})`;
-      });
-
-    return `## ${tag}\n${tagPaths.join('\n')}`;
-  }).join('\n\n');
+  // Generate a table of contents for paths based on HTTP methods
+  const tableOfContents = paths.map((path) => {
+    return `- [${path}](#${path.replace(/\//g, '-').replace(/{/g, '').replace(/}/g, '').toLowerCase()})`;
+  }).join('\n');
 
   // Generate the "Usage" section for each path based on HTTP methods
   let usageSection = '';
-  for (const [path, pathData] of Object.entries(doc.paths)) {
+  paths.forEach((path) => {
     usageSection += `\n## ${path}\n`;
-    
-    for (const [method, methodData] of Object.entries(pathData)) {
+    Object.keys(doc.paths[path]).forEach((method) => {
       if (method !== 'parameters') {
         usageSection += `### ${method}\n\n`;
-        
         // Add information about the method, such as request and response details
-        usageSection += 'Description: ' + methodData.summary + '\n';
-        
+        usageSection += `Description: ${doc.paths[path][method].summary}\n\n`;
         // You can add more details about the request and response here
-        
-        usageSection += '\n';
       }
-    }
-  }
+    });
+  });
 
-  // Customize your README content here using the 'doc', 'apiTitle', 'tableOfContents', and 'usageSection' variables
+  // Customize your README content here using the 'apiTitle', 'tableOfContents', and 'usageSection' variables
   const readmeContent = `
-# ${apiTitle} API Documentation
+  # ${apiTitle} API Documentation
 
-This documentation provides details about the ${apiTitle} API.
+  This documentation provides details about the ${apiTitle} API.
 
-## Table of Contents
-${tableOfContents}
+  ## Table of Contents
+  ${tableOfContents}
 
-## Usage
-${usageSection}
-`;
+  ## Usage
+  ${usageSection}
+  `;
 
   // Write the README.md file
   fs.writeFileSync('./README.md', readmeContent);
