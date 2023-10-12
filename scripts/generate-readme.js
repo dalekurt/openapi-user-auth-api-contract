0b1346f7eeaa5007ac07f2a8b498c0e6a62a22ce
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-12 12:58:33 -0400

    Update the README content

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -4,33 +4,62 @@ const yaml = require('js-yaml');
 try {
   const doc = yaml.load(fs.readFileSync('./openapi/openapi.yaml', 'utf8'));
 
+  // Define apiTitle with your API title from the OpenAPI file
+  const apiTitle = doc.info.title;
 
-  // Generate README content
+  // Extract tags from the OpenAPI file
+  const tags = doc.tags.map((tag) => tag.name);
+
+  // Generate a table of contents for paths based on tags
+  const tableOfContents = tags.map((tag) => {
+    const tagPaths = Object.entries(doc.paths)
+      .filter(([path, pathData]) => pathData.get.tags.includes(tag))
+      .map(([path, pathData]) => {
+        const operations = Object.keys(pathData);
+        return `- [${tag}](#${tag.toLowerCase()}-${path.replace(/\//g, '-').replace(/{/g, '').replace(/}/g, '').toLowerCase()}) (${operations.join(', ')})`;
+      });
+
+    return `## ${tag}\n${tagPaths.join('\n')}`;
+  }).join('\n\n');
+
+  // Generate the "Usage" section for each path based on HTTP methods
+  let usageSection = '';
+  for (const [path, pathData] of Object.entries(doc.paths)) {
+    usageSection += `\n## ${path}\n`;
+    
+    for (const [method, methodData] of Object.entries(pathData)) {
+      if (method !== 'parameters') {
+        usageSection += `### ${method}\n\n`;
+        
+        // Add information about the method, such as request and response details
+        usageSection += 'Description: ' + methodData.summary + '\n';
+        
+        // You can add more details about the request and response here
+        
+        usageSection += '\n';
+      }
+    }
+  }
+
+  // Customize your README content here using the 'doc', 'apiTitle', 'tableOfContents', and 'usageSection' variables
   const readmeContent = `
-  # ${apiTitle}
+  # ${apiTitle} API Documentation
 
-  **Version**: ${apiVersion}
+  This documentation provides details about the ${apiTitle} API.
 
-  ${apiDescription}
+  ## Table of Contents
+  ${tableOfContents}
 
-  ## Endpoints
+  ## Usage
+  ${usageSection}
 
-  ${pathDescriptions.join('\n')}
-
-  ## Example Usage
-
-  You can use this API to...
-
-  ## Changelog
-
-  - Version ${apiVersion}: Initial release
   `;
 
-  // Write the README content to a README.md file
-  fs.writeFileSync('README.md', readmeContent, 'utf8');
+  // Write the README.md file
+  fs.writeFileSync('./README.md', readmeContent);
 
   console.log('README.md generated successfully.');
 } catch (e) {
-  console.error('Error reading or generating the README.md:', e);
+  console.log('Error reading or generating the README.md: ' + e.message);
   process.exit(1);
 }
