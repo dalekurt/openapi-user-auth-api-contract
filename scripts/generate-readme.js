fcc9e431f66ac431193a0950007a425ff5d6f5a1
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-12 12:52:53 -0400

    Fix - dir path for openapi doc

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -2,22 +2,8 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 
 try {
-  // Read the OpenAPI YAML file
-  const openapiYAML = fs.readFileSync('openapi/openapi.yaml', 'utf8');
-  const openapiObject = yaml.safeLoad(openapiYAML);
+  const doc = yaml.load(fs.readFileSync('./openapi/openapi.yaml', 'utf8'));
 
-  // Extract information from the OpenAPI object
-  const apiTitle = openapiObject.info.title;
-  const apiVersion = openapiObject.info.version;
-  const apiDescription = openapiObject.info.description;
-
-  // Extract paths and descriptions
-  const paths = openapiObject.paths;
-  const pathDescriptions = Object.keys(paths).map((path) => {
-    const pathItem = paths[path];
-    const pathDescription = pathItem.get ? pathItem.get.summary : '';
-    return `  - ${path}: ${pathDescription}`;
-  });
 
   // Generate README content
   const readmeContent = `
