db119732ebdcccd172070dbcae14dcc234f02387
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 14:56:55 -0400

    Fix - Documentation formatting

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -1,17 +1,25 @@
 import json
 
-# Load the structured JSON from the redoc-cli output
-with open('structured.json', 'r') as json_file:
-    data = json.load(json_file)
+import yaml
+
+# Load the OpenAPI specification (assuming it's in YAML format)
+with open('openapi/openapi.yaml', 'r') as yaml_file:
+    openapi_spec = yaml.safe_load(yaml_file)
 
 # Define a function to format a single path
 def format_path(path, path_data):
     formatted_path = f'**{path}**\n----\n{path_data.get("description", "No description provided")}\n'
-    formatted_path += f'* **URL Params**\n{path_data.get("url_params", "None")}\n'
-    formatted_path += f'* **Data Params**\n{json.dumps(path_data.get("data_params", "None"), indent=2)}\n'
-    formatted_path += f'* **Headers**\n{path_data.get("headers", "None")}\n'
-    formatted_path += f'* **Success Response**\n{json.dumps(path_data.get("success_response", "None"), indent=2)}\n'
-    formatted_path += f'* **Error Response**\n{json.dumps(path_data.get("error_response", "None"), indent=2)}\n'
+    formatted_path += f'* **URL Params**\n{path_data.get("parameters", "None")}\n'
+    data_params = path_data.get("requestBody", "None")
+    if data_params != "None":
+        try:
+            json_data = json.loads(data_params)
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in requestBody: {e}")
+            # Handle the error as needed, e.g., provide a default JSON structure
+            json_data = {}
+        formatted_path += f'* **Data Params**\n```json\n{json.dumps(json_data, indent=2)}\n```\n'
+
     return formatted_path
 
 # Define a function to format the entire API documentation
@@ -19,20 +27,14 @@ def format_api(api_data):
     formatted_api = ""
     paths = api_data.get("paths", {})
     for path, path_data in paths.items():
-        # Ensure the path is not duplicated for multiple HTTP methods
-        unique_paths = set()
+        formatted_api += f'# {path_data.get("summary", "No summary provided")}\n'
         for method, method_data in path_data.items():
             if method not in ["parameters", "summary", "description"]:
-                if path not in unique_paths:
-                    formatted_api += f'**{path}**\n'
-                    unique_paths.add(path)
                 formatted_api += f'* **{method.upper()} {path}**\n'
                 formatted_api += format_path(path, method_data)
     return formatted_api
 
-# Transform the structured JSON into the desired Markdown format
-markdown_content = format_api(data)
+# Transform the OpenAPI specification into the desired Markdown format
+markdown_content = format_api(openapi_spec)
 
 # Write the formatted content to the README.md file
-with open('README.md', 'w') as readme_file:
-    readme_file.write(markdown_content)
