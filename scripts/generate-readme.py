0dfef1a88cf959b3059c7ff30013f6c0d43068d7
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 18:24:09 -0400

    Fix - Documentation formatting on format_path

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -8,13 +8,13 @@ with open('openapi/openapi.yaml', 'r') as yaml_file:
 
 # Define a function to format a single path
 def format_path(path, path_data):
-    formatted_path = f'**{path_data.get("summary", "No summary provided")} (ex {path})**\n----\n'
-    formatted_path += f'**The description (ex {path_data.get("description", "No description provided")})**\n'
+    formatted_path = f'**{path_data.get("summary", "No summary provided")}**\n----\n'
+    formatted_path += f'  {path_data.get("description", "No description provided")}.\n'
     formatted_path += f'* **URL Params**\n'
 
     parameters = path_data.get("parameters", [])
     if parameters:
-        formatted_path += '  *Required:* ' + ', '.join([f'{param["name"]}' + (f'[{param["type"]}]' if "type" in param else '') for param in parameters]) + '\n'
+        formatted_path += '  *Required:* ' + ', '.join([f'`{param["name"]}' + (f'=[{param.get("type", "")}]`' if "type" in param else '`[type]`') for param in parameters]) + '\n'
     else:
         formatted_path += '  None\n'
 
@@ -29,7 +29,21 @@ def format_path(path, path_data):
     else:
         formatted_path += '  None\n'
 
-    # Add more sections as needed, e.g., headers, responses
+    formatted_path += '* **Headers**\n'
+    formatted_path += '  Content-Type: application/json\n'
+    formatted_path += '  Authorization: Bearer `<OAuth Token>`\n'
+    
+    formatted_path += '* **Success Response:**\n'
+    formatted_path += '* **Code:** 200\n'
+    formatted_path += '  **Content:**  `{ <user_object> }`\n'
+    
+    formatted_path += '* **Error Response:**\n'
+    formatted_path += '  * **Code:** 404\n'
+    formatted_path += '  **Content:** `{ error : "User doesn\'t exist" }`\n'
+    formatted_path += '  OR\n'
+    formatted_path += '  * **Code:** 401\n'
+    formatted_path += '  **Content:** `{ error : error : "You are unauthorized to make this request." }`\n'
+
     return formatted_path
 
 # Define a function to format the entire API documentation
@@ -37,35 +51,12 @@ def format_api(api_data):
     formatted_api = ""
     paths = api_data.get("paths", {})
     for path, path_data in paths.items():
-        formatted_api += f'**{path_data.get("summary", "No summary provided")} (ex {path})**\n----\n'
-        formatted_api += f'**The description (ex {path_data.get("description", "No description provided")})**\n'
-        formatted_api += f'* **URL Params**\n'
-
-        parameters = path_data.get("parameters", [])
-        if parameters:
-            formatted_api += '  *Required:* ' + ', '.join([f'{param["name"]}' + (f'[{param.get("type", "")}]' if "type" in param else '') for param in parameters]) + '\n'
-        else:
-            formatted_api += '  None\n'
-
-        formatted_api += '* **Data Params**\n'
-        if "requestBody" in path_data:
-            try:
-                # Serialize the dictionary to a JSON-formatted string
-                json_data = json.dumps(path_data["requestBody"], indent=2)
-                formatted_api += f'  ```json\n{json_data}\n  ```\n'
-            except json.JSONDecodeError:
-                formatted_api += '  None\n'
-        else:
-            formatted_api += '  None\n'
-
-        # Add more sections as needed, e.g., headers, responses
+        formatted_api += format_path(path, path_data)
 
     return formatted_api
 
-
 # Transform the OpenAPI specification into the desired Markdown format
 markdown_content = format_api(openapi_spec)
-
 
 # Write the formatted content to the README.md file
 with open('README.md', 'w') as readme_file:
