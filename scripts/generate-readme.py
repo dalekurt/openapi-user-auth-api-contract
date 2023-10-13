63dac7a6f2af6f573e49105388a88e0ce3264652
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 19:06:07 -0400

    Updated the format for each method

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -15,8 +15,8 @@ contact_email = openapi_data.get('info', {}).get('contact', {}).get('email', 'co
 
 license = openapi_data.get('info', {}).get('license', {}).get('name', 'License Name')
 
-# Define a function to generate the API documentation
-def generate_api_documentation(openapi_data):
+# Define a function to generate the API documentation for a specific path and method
+def generate_method_documentation(path, method_data):
     documentation = []
 
     # Add title, version, and description
@@ -32,32 +32,53 @@ def generate_api_documentation(openapi_data):
     # Add license information
     documentation.append(f"License: {license}\n")
 
+    # Extract method-specific information
+    method = method_data['method']
+    method_summary = method_data.get('summary', 'No summary available')
+    method_description = method_data.get('description', 'No description available')
+
+    documentation.append(f"## {method} {path}")
+
+    # Add method-specific summary and description
+    documentation.append(f"{method_summary}\n")
+    documentation.append(f"{method_description}\n")
+
+    # Input parameters
+    if 'parameters' in method_data:
+        documentation.append("Input parameters:")
+        for param in method_data['parameters']:
+            param_name = param['name']
+            param_description = param.get('description', 'No description available')
+            documentation.append(f"- {param_name} ({param['in']}): {param_description}")
+
+    # Output
+    if 'responses' in method_data:
+        documentation.append("Output:")
+        for status_code, response in method_data['responses'].items():
+            if 'description' in response:
+                documentation.append(f"- {status_code} ({response['description']})")
+
+    documentation.append("Example usage:")
+    documentation.append(f"{method} {path}\n")
+
+    if 'responses' in method_data:
+        for status_code, response in method_data['responses'].items():
+            if 'example' in response:
+                documentation.append(response['example'])
+
+    documentation.append("\n")
+
+    return "\n".join(documentation)
+
+# Define a function to generate the full API documentation
+def generate_api_documentation(openapi_data):
+    documentation = []
+
     # Loop through the paths in the OpenAPI definition
     for path, path_data in openapi_data['paths'].items():
-        documentation.append(f"{path}\n")
-
-        if 'parameters' in path_data.get('get', {}):
-            documentation.append("Input parameters:")
-            for param in path_data['get']['parameters']:
-                param_name = param['name']
-                param_description = param.get('description', 'No description available')
-                documentation.append(f"- {param_name} ({param['in']}): {param_description}")
-
-        if 'responses' in path_data.get('get', {}):
-            documentation.append("Output:")
-            for status_code, response in path_data['get']['responses'].items():
-                if 'description' in response:
-                    documentation.append(f"- {status_code} ({response['description']})")
-
-        documentation.append("Example usage:")
-        documentation.append(f"{path}\n")
-
-        if 'responses' in path_data.get('get', {}):
-            for status_code, response in path_data['get']['responses'].items():
-                if 'example' in response:
-                    documentation.append(response['example'])
-
-        documentation.append("\n")
+        for method, method_data in path_data.items():
+            method_data['method'] = method.upper()
+            documentation.append(generate_method_documentation(path, method_data))
 
     return "\n".join(documentation)
 
