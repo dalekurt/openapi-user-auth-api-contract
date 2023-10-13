ced98a395af00b63bfb1a944652469b5450d463c
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 00:10:04 -0400

    Remove node_modules

--- scripts/generate-readme.js
+++ scripts/generate-readme.js
@@ -1,52 +1,34 @@
 const fs = require('fs');
-const yaml = require('js-yaml');
+const swaggerJSDoc = require('swagger-jsdoc');
+const markdownTable = require('markdown-table');
 
-try {
-  const doc = yaml.load(fs.readFileSync('./openapi/openapi.yaml', 'utf8'));
+// Define options for swagger-jsdoc
+const options = {
+  definition: {
+    openapi: '3.0.0', // specify the OpenAPI version
+    info: {
+      title: 'Your API Title',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./openapi/openapi.yaml'], // path to your OpenAPI file
+};
 
-  // Define apiTitle with your API title from the OpenAPI file
-  const apiTitle = doc.info.title;
+const swaggerSpec = swaggerJSDoc(options);
 
-  // Extract paths from the OpenAPI file
-  const paths = Object.keys(doc.paths);
+// Generate a markdown table from the Swagger spec
+const table = markdownTable([
+  ['Endpoint', 'Method', 'Description'],
+  ...swaggerSpec.paths.map((path, pathName) =>
+    Object.entries(path).map(([method, endpoint]) => [
+      pathName,
+      method.toUpperCase(),
+      endpoint.summary,
+    ])
+  ),
+]);
 
-  // Generate a table of contents for paths based on HTTP methods
-  const tableOfContents = paths.map((path) => {
-    return `- [${path}](#${path.replace(/\//g, '-').replace(/{/g, '').replace(/}/g, '').toLowerCase()})`;
-  }).join('\n');
+// Write the table to the README.md file
+fs.writeFileSync('./README.md', table);
 
-  // Generate the "Usage" section for each path based on HTTP methods
-  let usageSection = '';
-  paths.forEach((path) => {
-    usageSection += `\n## ${path}\n`;
-    Object.keys(doc.paths[path]).forEach((method) => {
-      if (method !== 'parameters') {
-        usageSection += `### ${method}\n\n`;
-        // Add information about the method, such as request and response details
-        usageSection += `Description: ${doc.paths[path][method].summary}\n\n`;
-        // You can add more details about the request and response here
-      }
-    });
-  });
-
-  // Customize your README content here using the 'apiTitle', 'tableOfContents', and 'usageSection' variables
-  const readmeContent = `
-  # ${apiTitle} API Documentation
-
-  This documentation provides details about the ${apiTitle} API.
-
-  ## Table of Contents
-  ${tableOfContents}
-
-  ## Usage
-  ${usageSection}
-  `;
-
-  // Write the README.md file
-  fs.writeFileSync('./README.md', readmeContent);
-
-  console.log('README.md generated successfully.');
-} catch (e) {
-  console.log('Error reading or generating the README.md: ' + e.message);
-  process.exit(1);
-}
+console.log('README generated successfully');
