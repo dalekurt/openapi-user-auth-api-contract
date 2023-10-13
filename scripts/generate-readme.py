61d86d2a529d3e6a3563291c2461cfa25013b6fd
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 14:48:45 -0400

    Fix - Documentation formatting

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -1,15 +1,17 @@
-import yaml
+import json
 
-# Load the OpenAPI specification (assuming it's in YAML format)
-with open('openapi/openapi.yaml', 'r') as yaml_file:
-    openapi_spec = yaml.safe_load(yaml_file)
+# Load the structured JSON from the redoc-cli output
+with open('structured.json', 'r') as json_file:
+    data = json.load(json_file)
 
 # Define a function to format a single path
 def format_path(path, path_data):
     formatted_path = f'**{path}**\n----\n{path_data.get("description", "No description provided")}\n'
-    formatted_path += f'* **URL Params**\n{path_data.get("parameters", "None")}\n'
-    formatted_path += f'* **Data Params**\n{path_data.get("requestBody", "None")}\n'
-    # Add more sections as needed, e.g., headers, responses
+    formatted_path += f'* **URL Params**\n{path_data.get("url_params", "None")}\n'
+    formatted_path += f'* **Data Params**\n{json.dumps(path_data.get("data_params", "None"), indent=2)}\n'
+    formatted_path += f'* **Headers**\n{path_data.get("headers", "None")}\n'
+    formatted_path += f'* **Success Response**\n{json.dumps(path_data.get("success_response", "None"), indent=2)}\n'
+    formatted_path += f'* **Error Response**\n{json.dumps(path_data.get("error_response", "None"), indent=2)}\n'
     return formatted_path
 
 # Define a function to format the entire API documentation
@@ -17,15 +19,19 @@ def format_api(api_data):
     formatted_api = ""
     paths = api_data.get("paths", {})
     for path, path_data in paths.items():
-        formatted_api += f'# {path_data.get("summary", "No summary provided")}\n'
+        # Ensure the path is not duplicated for multiple HTTP methods
+        unique_paths = set()
         for method, method_data in path_data.items():
             if method not in ["parameters", "summary", "description"]:
+                if path not in unique_paths:
+                    formatted_api += f'**{path}**\n'
+                    unique_paths.add(path)
                 formatted_api += f'* **{method.upper()} {path}**\n'
                 formatted_api += format_path(path, method_data)
     return formatted_api
 
-# Transform the OpenAPI specification into the desired Markdown format
-markdown_content = format_api(openapi_spec)
+# Transform the structured JSON into the desired Markdown format
+markdown_content = format_api(data)
 
 # Write the formatted content to the README.md file
 with open('README.md', 'w') as readme_file:
