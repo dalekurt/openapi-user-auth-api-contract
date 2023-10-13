64cf80d1858d0fd65d972e4575c47cf28294b89e
Dale-Kurt Murray <hello@dalekurtmurray.com>
2023-10-13 19:02:42 -0400

    Updated the format

--- scripts/generate-readme.py
+++ scripts/generate-readme.py
@@ -1,66 +1,69 @@
-import json
-
 import yaml
 
-# Load the OpenAPI specification (assuming it's in YAML format)
-with open('openapi/openapi.yaml', 'r') as yaml_file:
-    openapi_spec = yaml.safe_load(yaml_file)
+# Load the OpenAPI specification from the file
+with open('openapi/openapi.yaml', 'r') as openapi_file:
+    openapi_data = yaml.safe_load(openapi_file)
 
-# Define a function to format a single path
-def format_path(path, method, path_data):
-    formatted_path = f'**{method} {path}**\n----\n'
-    formatted_path = f'**{path_data.get("summary", "No summary provided")}**\n----\n'
-    formatted_path += f'  {path_data.get("description", "No description provided")}.\n'
-    formatted_path += f'* **URL Params**\n'
+# Extract relevant information from the OpenAPI definition
+title = openapi_data.get('info', {}).get('title', 'API Title')
+version = openapi_data.get('info', {}).get('version', '1.0.0')
+summary = openapi_data.get('info', {}).get('summary', 'API Summary')
+description = openapi_data.get('info', {}).get('description', 'API Description')
 
-    parameters = path_data.get("parameters", [])
-    if parameters:
-        formatted_path += '  *Required:* ' + ', '.join([f'`{param["name"]}' + (f'=[{param.get("type", "")}]`' if "type" in param else '`[type]`') for param in parameters]) + '\n'
-    else:
-        formatted_path += '  None\n'
+contact_name = openapi_data.get('info', {}).get('contact', {}).get('name', 'Contact Name')
+contact_email = openapi_data.get('info', {}).get('contact', {}).get('email', 'contact@example.com')
 
-    formatted_path += '* **Data Params**\n'
-    if "requestBody" in path_data:
-        try:
-            # Serialize the dictionary to a JSON-formatted string
-            json_data = json.dumps(path_data["requestBody"], indent=2)
-            formatted_path += f'  ```json\n{json_data}\n  ```\n'
-        except json.JSONDecodeError:
-            formatted_path += '  None\n'
-    else:
-        formatted_path += '  None\n'
+license = openapi_data.get('info', {}).get('license', {}).get('name', 'License Name')
 
-    formatted_path += '* **Headers**\n'
-    formatted_path += '  Content-Type: application/json\n'
-    formatted_path += '  Authorization: Bearer `<OAuth Token>`\n'
-    
-    formatted_path += '* **Success Response:**\n'
-    formatted_path += '* **Code:** 200\n'
-    formatted_path += '  **Content:**  `{ <user_object> }`\n'
-    
-    formatted_path += '* **Error Response:**\n'
-    formatted_path += '  * **Code:** 404\n'
-    formatted_path += '  **Content:** `{ error : "User doesn\'t exist" }`\n'
-    formatted_path += '  OR\n'
-    formatted_path += '  * **Code:** 401\n'
-    formatted_path += '  **Content:** `{ error : error : "You are unauthorized to make this request." }`\n'
+# Define a function to generate the API documentation
+def generate_api_documentation(openapi_data):
+    documentation = []
 
-    return formatted_path
+    # Add title, version, and description
+    documentation.append(f"# {title} (Version: {version})")
+    documentation.append(f"{summary}\n")
+    documentation.append(f"{description}\n")
 
-# Define a function to format the entire API documentation
-def format_api(api_data):
-    formatted_api = ""
-    paths = api_data.get("paths", {})
-    for path, path_data in paths.items():
-        for method, method_data in path_data.items():
-            if method not in ["parameters", "summary", "description"]:
-                formatted_api += format_path(path, method, method_data)
+    # Add contact information
+    documentation.append("Contact:")
+    documentation.append(f"- Name: {contact_name}")
+    documentation.append(f"- Email: {contact_email}\n")
 
-    return formatted_api
+    # Add license information
+    documentation.append(f"License: {license}\n")
 
-# Transform the OpenAPI specification into the desired Markdown format
-markdown_content = format_api(openapi_spec)
+    # Loop through the paths in the OpenAPI definition
+    for path, path_data in openapi_data['paths'].items():
+        documentation.append(f"{path}\n")
 
-# Write the formatted content to the README.md file
+        if 'parameters' in path_data.get('get', {}):
+            documentation.append("Input parameters:")
+            for param in path_data['get']['parameters']:
+                param_name = param['name']
+                param_description = param.get('description', 'No description available')
+                documentation.append(f"- {param_name} ({param['in']}): {param_description}")
+
+        if 'responses' in path_data.get('get', {}):
+            documentation.append("Output:")
+            for status_code, response in path_data['get']['responses'].items():
+                if 'description' in response:
+                    documentation.append(f"- {status_code} ({response['description']})")
+
+        documentation.append("Example usage:")
+        documentation.append(f"{path}\n")
+
+        if 'responses' in path_data.get('get', {}):
+            for status_code, response in path_data['get']['responses'].items():
+                if 'example' in response:
+                    documentation.append(response['example'])
+
+        documentation.append("\n")
+
+    return "\n".join(documentation)
+
+# Generate the documentation
+api_documentation = generate_api_documentation(openapi_data)
+
+# Write the documentation to the README file
 with open('README.md', 'w') as readme_file:
-    readme_file.write(markdown_content)
+    readme_file.write(api_documentation)
